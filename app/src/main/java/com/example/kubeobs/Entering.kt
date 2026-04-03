d5eda395fd9a8a24f17cf193cb1740d82ca7d975
dest-1ny <romansioli89@gmail.com>
2026-04-04 02:24:13 +0300

    starter-anim

--- app/src/main/java/com/example/kubeobs/Entering.kt
+++ app/src/main/java/com/example/kubeobs/Entering.kt
@@ -1,13 +1,19 @@
 package com.example.kubeobs
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,105 +29,221 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import kotlin.math.roundToInt
 
 
 @Composable
 fun EnteringScreen(navController: NavController){
-    val username: TextFieldState = rememberTextFieldState()
-    val password: TextFieldState = rememberTextFieldState()
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Text(
-            text = "Log In",
-            modifier = Modifier
-                .padding(top=50.dp),
-            fontSize = 42.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-        )
+    var isSignUpDisplays by remember { mutableStateOf(false) }
+    val pxToMove = with(LocalDensity.current) {
+        1000.dp.toPx().roundToInt()
+    }
+    val offset by animateIntOffsetAsState(
+        targetValue = if (isSignUpDisplays) {
+            IntOffset(0, pxToMove)
+        } else {
+            IntOffset.Zero
+        },
+        label = "offset"
+    )
+
+    AnimatedVisibility(!isSignUpDisplays) {
+        val username: TextFieldState = rememberTextFieldState()
+        val password: TextFieldState = rememberTextFieldState()
         Column(
             modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth(),
+                .offset{
+                    offset
+                }
+                .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ){
-            OutlinedTextField(
-                state = username,
-                lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
-                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
-                label = { Text("Username") },
-                colors = OutlinedTextFieldDefaults.colors(
-                    disabledBorderColor = Color(Colors.kubeColor),
-                    errorBorderColor = Color(Colors.kubeColor),
-                    focusedBorderColor = Color(Colors.kubeColor),
-                    unfocusedBorderColor = Color(Colors.kubeColor)
-                ),
-                shape = RoundedCornerShape(10.dp)
-            )
-            OutlinedTextField(
-                state = password,
-                lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
-                textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
-                label = { Text("Password") },
-                colors = OutlinedTextFieldDefaults.colors(
-                    disabledBorderColor = Color(Colors.kubeColor),
-                    errorBorderColor = Color(Colors.kubeColor),
-                    focusedBorderColor = Color(Colors.kubeColor),
-                    unfocusedBorderColor = Color(Colors.kubeColor)
-                ),
-                shape = RoundedCornerShape(10.dp)
-            )
-            Spacer(
+        ) {
+            Text(
+                text = "Log In",
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(30.dp)
+                    .padding(top=50.dp),
+                fontSize = 42.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
             )
-            Button(
-                colors = ButtonColors(
-                    containerColor = Color(Colors.kubeColor),
-                    contentColor = Color.White,
-                    disabledContainerColor = Color(Colors.kubeColor),
-                    disabledContentColor = Color.White
-                ),
+            Column(
                 modifier = Modifier
-                    .size(
-                        height = 50.dp,
-                        width = 150.dp
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                OutlinedTextField(
+                    state = username,
+                    lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
+                    textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                    label = { Text("Username") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledBorderColor = Color(Colors.kubeColor),
+                        errorBorderColor = Color(Colors.kubeColor),
+                        focusedBorderColor = Color(Colors.kubeColor),
+                        unfocusedBorderColor = Color(Colors.kubeColor)
                     ),
-                onClick = {navController.navigate(Routes.MainScreen)}
+                    shape = RoundedCornerShape(10.dp)
+                )
+                OutlinedTextField(
+                    state = password,
+                    lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
+                    textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                    label = { Text("Password") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledBorderColor = Color(Colors.kubeColor),
+                        errorBorderColor = Color(Colors.kubeColor),
+                        focusedBorderColor = Color(Colors.kubeColor),
+                        unfocusedBorderColor = Color(Colors.kubeColor)
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(30.dp)
+                )
+                Button(
+                    colors = ButtonColors(
+                        containerColor = Color(Colors.kubeColor),
+                        contentColor = Color.White,
+                        disabledContainerColor = Color(Colors.kubeColor),
+                        disabledContentColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .size(
+                            height = 50.dp,
+                            width = 150.dp
+                        ),
+                    onClick = {navController.navigate(Routes.MainScreen)}
+                ) {
+                    Text(
+                        text = "Log In",
+                        fontSize = 22.sp
+                    )
+                }
+            }
+            Text(
+                text = "Have no account?",
+                color = Color.Black,
+            )
+            TextButton(
+                onClick = {isSignUpDisplays=!isSignUpDisplays},
+                modifier = Modifier
+                    .padding(bottom = 30.dp),
             ) {
                 Text(
-                    text = "Log In",
-                    fontSize = 22.sp
+                    text = "Sign up",
+                    color = Color(Colors.kubeColor),
                 )
             }
         }
-        Text(
-            text = "Have no account?",
-            color = Color.Black,
-        )
-        TextButton(
-            onClick = {},
+    }
+    AnimatedVisibility(isSignUpDisplays) {
+        val username: TextFieldState = rememberTextFieldState()
+        val password: TextFieldState = rememberTextFieldState()
+
+        Column(
             modifier = Modifier
-                .padding(bottom = 30.dp),
-            ) {
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             Text(
-                text = "Sign up",
-                color = Color(Colors.kubeColor),
+                text = "Sign Up",
+                modifier = Modifier
+                    .padding(top=50.dp),
+                fontSize = 42.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
             )
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ){
+                OutlinedTextField(
+                    state = username,
+                    lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
+                    textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                    label = { Text("Username") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledBorderColor = Color(Colors.kubeColor),
+                        errorBorderColor = Color(Colors.kubeColor),
+                        focusedBorderColor = Color(Colors.kubeColor),
+                        unfocusedBorderColor = Color(Colors.kubeColor)
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                OutlinedTextField(
+                    state = password,
+                    lineLimits = TextFieldLineLimits.MultiLine(maxHeightInLines = 2),
+                    textStyle = TextStyle(color = Color.Black, fontWeight = FontWeight.Bold),
+                    label = { Text("Password") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        disabledBorderColor = Color(Colors.kubeColor),
+                        errorBorderColor = Color(Colors.kubeColor),
+                        focusedBorderColor = Color(Colors.kubeColor),
+                        unfocusedBorderColor = Color(Colors.kubeColor)
+                    ),
+                    shape = RoundedCornerShape(10.dp)
+                )
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(30.dp)
+                )
+                Button(
+                    colors = ButtonColors(
+                        containerColor = Color(Colors.kubeColor),
+                        contentColor = Color.White,
+                        disabledContainerColor = Color(Colors.kubeColor),
+                        disabledContentColor = Color.White
+                    ),
+                    modifier = Modifier
+                        .size(
+                            height = 50.dp,
+                            width = 150.dp
+                        ),
+                    onClick = {navController.navigate(Routes.MainScreen)}
+                ) {
+                    Text(
+                        text = "Sign Up",
+                        fontSize = 22.sp
+                    )
+                }
+            }
+            Text(
+                text = "Have an account?",
+                color = Color.Black,
+            )
+            TextButton(
+                onClick = {isSignUpDisplays=!isSignUpDisplays},
+                modifier = Modifier
+                    .padding(bottom = 30.dp),
+            ) {
+                Text(
+                    text = "Log In",
+                    color = Color(Colors.kubeColor),
+                )
+            }
         }
     }
 }

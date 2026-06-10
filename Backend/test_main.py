0e14afe416dad24bf62448e1dbb7f8e4d048fd08
serva <583leonid.kovalchuk@gmail.com>
2026-06-10 22:31:41 +0000

    Updating tests

--- Backend/test_main.py
+++ Backend/test_main.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
-
+#
 def test_security_unauthorized_access():
     response = client.get("/nodes")
     assert response.status_code == 401

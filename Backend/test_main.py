79baa538740422e0475eecb0c4890b217e390bf4
serva <583leonid.kovalchuk@gmail.com>
2026-06-11 12:08:16 +0000

    test: update expected unauthorized message for http bearer

--- Backend/test_main.py
+++ Backend/test_main.py
@@ -2,10 +2,9 @@ from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
-#
+
+
 def test_security_unauthorized_access():
     response = client.get("/nodes")
     assert response.status_code == 401
-    assert response.json() == {"detail": "Error: Unauthorized access"}
-
-#
+    assert response.json() == {"detail": "Not authenticated"}
